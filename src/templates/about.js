b5ce10495efc861b014e4cb0069928a7226aa663
David Konsumer <konsumer@jetboystudio.com>
2017-10-28 09:36:41 -0700

    better about page

--- src/templates/about.js
+++ src/templates/about.js
@@ -4,19 +4,10 @@ import graphql from 'graphql'
 export default ({ data }) => {
   const { markdownRemark: post } = data
   return (
-    <section className='section section--gradient'>
-      <div className='container'>
-        <div className='columns'>
-          <div className='column is-7'>
-            <div className='section'>
-              <h2 className='title is-size-3 has-text-primary is-bold-light'>{post.frontmatter.title}</h2>
-              <div className='content' dangerouslySetInnerHTML={{ __html: post.html }} />
-            </div>
-          </div>
-          <div className='column is-5' />
-        </div>
-      </div>
-    </section>
+    <div className='about'>
+      <h2 className='title is-size-3 has-text-primary is-bold-light'>{post.frontmatter.title}</h2>
+      <div className='content' dangerouslySetInnerHTML={{ __html: post.html }} />
+    </div>
   )
 }
 
