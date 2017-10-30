f039325bef50d868a7cd014707f83b030670806a
David Konsumer <konsumer@jetboystudio.com>
2017-10-30 10:31:43 -0700

    disabled image display and use title from config

--- src/templates/image.js
+++ src/templates/image.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import Helmet from 'react-helmet'
 
 // dummy, images are not show directly.
 export default function Template () {
-  return (
-    null
-  )
+  return null
 }
